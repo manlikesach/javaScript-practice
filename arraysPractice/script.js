84e2d1b547d546facd53b23724eca7de4acf17b2
manlikesach <95724242+manlikesach@users.noreply.github.com>
2025-06-10 15:15:20 +1000

    Added toCamelCase function for kebab-case conversion

--- arraysPractice/script.js
+++ arraysPractice/script.js
@@ -1,16 +1,28 @@
-// Take input strings(eg: “border-left-width”).
-let str = "border-left-width";
+// // Take input strings(eg: “border-left-width”).
+// let str = "border-left-width";
 
-// Split the string into an array.
-let parts = str.split("-");
+// // Split the string into an array.
+// let parts = str.split("-");
 
-// Capitalise first letter of words excluding frist.
-for (let i = 1; i < parts.length; i++) {
-  parts[i] = parts[i][0].toUpperCase() + parts[i].slice(1);
+// // Capitalise first letter of words excluding frist.
+// for (let i = 1; i < parts.length; i++) {
+//   parts[i] = parts[i][0].toUpperCase() + parts[i].slice(1);
+// }
+
+// // Join all letters back together into one string.
+// let camelCaseStr = parts.join("");
+
+// // Print output
+// console.log(camelCaseStr);
+
+// Turning to reusable function
+function toCamelCase(str) {
+  let parts = str.split("-");
+
+  for (let i = 1; i < parts.length; i++) {
+    parts[i] = parts[i][0].toUpperCase() + parts[i].slice(1);
+  }
+  return parts.join("");
 }
 
-// Join all letters back together into one string.
-let camelCaseStr = parts.join("");
-
-// Print output
-console.log(camelCaseStr);
+console.log(toCamelCase("list-style-image"));
