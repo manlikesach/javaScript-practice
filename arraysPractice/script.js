bbc81b9d1b698321bda02eb204839c948ab91dd6
manlikesach <95724242+manlikesach@users.noreply.github.com>
2025-06-10 14:54:45 +1000

    Completed camelCase converter from kebab-case

---
+++ arraysPractice/script.js
@@ -0,0 +1,16 @@
+// Take input strings(eg: “border-left-width”).
+let str = "border-left-width";
+
+// Split the string into an array.
+let parts = str.split("-");
+
+// Capitalise first letter of words excluding frist.
+for (let i = 1; i < parts.length; i++) {
+  parts[i] = parts[i][0].toUpperCase() + parts[i].slice(1);
+}
+
+// Join all letters back together into one string.
+let camelCaseStr = parts.join("");
+
+// Print output
+console.log(camelCaseStr);
