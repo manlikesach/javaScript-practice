298d336555a63dad812c3832a0f36da81b5c16ab
manlikesach <95724242+manlikesach@users.noreply.github.com>
2025-06-11 10:57:38 +1000

    Implemented copySorted function using slice and sort

--- arraysPractice/sort.js
+++ arraysPractice/sort.js
@@ -1,5 +1,30 @@
-let arr = [5, 2, 1, -10, 8];
+// let arr = [5, 2, 1, -10, 8];
 
-arr.sort((a, b) => b - a);
+// arr.sort((a, b) => b - a);
 
-console.log(arr); // 8, 5, 2, 1, -10
+// console.log(arr); // 8, 5, 2, 1, -10
+
+// Sorted copy method
+
+let arr = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Java",
+  "Python",
+  "Go",
+  "Assembly",
+  "Rust",
+  "C",
+  "C++",
+  "Ruby",
+];
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+sorted = copySorted(arr);
+
+console.log(arr);
+console.log(sorted);
